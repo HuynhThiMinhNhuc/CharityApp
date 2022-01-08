ddeace036b0338745691f09f5af17b9206b1d55f
UIT.19520266 <19520266@gm.uit.edu.vn>
2022-01-08 20:55:03 +0700

    add loading for form

---
+++ CharityApp/lib/views/Component/loading_circular_indicator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoaddingCircularIndicator extends StatelessWidget {
+  const LoaddingCircularIndicator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CircularProgressIndicator(strokeWidth: 2,),
+    );
+  }
+}
